0806af31408d56055869cc52c4573b95e6f0b99d
Daulet <daulet.bakeuly@gmail.com>
2021-04-04 19:24:22 +0600

    feat: add alt-s to img to corespond rule 1.2 in extended guideline

--- app.js
+++ app.js
@@ -52,6 +52,7 @@ var createNewTaskElement=function(taskString){
     deleteButton.setAttribute("styling", "basic-button");
     deleteButtonImg.src='./remove.svg';
     deleteButtonImg.className = "delete-img";
+    deleteButtonImg.setAttribute("alt", "delete-button");
     deleteButton.appendChild(deleteButtonImg);
 
 
